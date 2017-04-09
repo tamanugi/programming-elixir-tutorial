b939f927f5b18f490344a463d36c8a0b6f5ae7a1
tamanugi <tamanugi@gmail.com>
2017-04-09 16:56:01 +0900

    全体のスーパーバイザを実装

---
+++ 17_supervisor/sequence/lib/sequence/supervisor.ex
@@ -0,0 +1,23 @@
+defmodule Sequence.Supervisor do
+  use Supervisor
+
+  def start_link(initial_number) do
+    result = {:ok, sup} = Supervisor.start_link(__MODULE__, [initial_number])
+    start_workers(sup, initial_number)
+    result
+  end
+
+  def start_workers(sup, initial_number) do
+    # スタッシュワーカーを開始
+    {:ok, stash} =
+      Supervisor.start_child(sup, worker(Sequence.Stash, [initial_number]))
+
+    # そして、実際のsequenceサーバのスーパーバイザを開始
+    Supervisor.start_child(sup, supervisor(Sequence.SubSupervisor, [stash]))
+  end
+
+  def init(_) do
+    supervise [], strategy: :one_for_one
+  end
+
+end
