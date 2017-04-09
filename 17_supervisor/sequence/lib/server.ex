1765bed9af07085a896f362c1ad6da51740f437f
tamanugi <tamanugi@gmail.com>
2017-04-09 16:49:11 +0900

    Sequenceをサンプル通りに実装

---
+++ 17_supervisor/sequence/lib/server.ex
@@ -0,0 +1,31 @@
+defmodule Sequence.Server do
+  use GenServer
+
+  ## 外部APIの実装
+
+  def start_link(current_number) do
+    GenServer.start_link(__MODULE__, current_number, name: __MODULE__)
+  end
+
+  def next_number do
+    GenServer.call __MODULE__, :next_number
+  end
+
+  def increment_number(delta) do
+    GenServer.cast __MODULE__, {:increment_number, delta}
+  end
+
+  ## GenSererの実装
+
+  def handle_call(:next_number, _from, current_number) do
+    { :reply, current_number, current_number+1 }
+  end
+
+  def handle_call({:set_number, new_number}, _from, _current_number) do
+    { :reply, new_number, new_number}
+  end
+
+  def handle_cast({:increment_number, delta}, current_number) do
+    { :noreply, current_number + delta}
+  end
+end
